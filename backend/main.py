d8ccc4635dad5f2fd61dded4d6baa1cdee5090bb
ev263 <ramjeet.prajapati@evervent.in>
2026-02-22 13:43:27 +0000

    test app with local dns

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://libraryapp.com",
+        "https://libraryapp.com",
+
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
